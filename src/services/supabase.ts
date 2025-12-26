ed780482d9cbccf77c92346082fcf517f4bb6185
Isaacayomi <okunlolaa3@outlook.com>
2025-12-26 17:11:34 +0100

    feat: update Supabase client to use environment variables

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vsbwpwnjxcbsifvltgof.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzYndwd25qeGNic2lmdmx0Z29mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwNTc2NjksImV4cCI6MjA3MzYzMzY2OX0.nC1Y7fkBxXsC-vA5XUkXlrVbbFdpU2FyRiuLXlIEH-o";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase environment variables");
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
