fff6e3614b4d55331587c44a1f67e4fec24c7276
Isaacayomi <okunlolaa3@outlook.com>
2026-07-11 23:05:52 +0100

    build config and dependency updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ export default {
         grayishBlue: "#5A698F",
         semiDarkBlue: "#161D2F",
       },
+      keyframes: {
+        "scroll-up": {
+          "0%": { transform: "translateY(0)" },
+          "100%": { transform: "translateY(-50%)" },
+        },
+        "scroll-down": {
+          "0%": { transform: "translateY(-50%)" },
+          "100%": { transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "scroll-up": "scroll-up 40s linear infinite",
+        "scroll-down": "scroll-down 40s linear infinite",
+      },
     },
   },
   plugins: [],
